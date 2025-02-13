5452fec792225c3cd91997cf17f801e284cdf9fd
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-13 15:58:52 -0600

    I added two  instance variables- registered_vehicles and collected_fees  and assigned each of them to an empty array. I also defined a new method called register_vehicle and passed in the vehicle object called camaro

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,24 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registred_vehicles, :collected_fees
 
   def initialize(facility_data)
     @name = facility_data[:name]
     @address = facility_data[:address]
     @phone = facility_data[:phone]
     @services = []
+    @registred_vehicles = []
+    @collected_fees = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @registred_vehicles << vehicle  
+  end
+
+  def count_collected_fees
+    
   end
 end
