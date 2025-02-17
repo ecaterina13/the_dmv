7c892fa51b0df44430866f672aaf887184dd2ada
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-16 19:32:04 -0600

    I created a method called create_facilities in order parse over the data set and create objects using to Facility arguments.

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,10 +4,19 @@ class FacilityFactory
   end
 
 
-  def create_facilities(locations)
-     locations.map do |location|
-     binding.pry
-     facility = Facility.new({name: location[dmv_office], address: location[address_li], phone: location[phone], services: location[serices_p] })
+  def create_facilities(locations,source)
+    locations.map do |location|
+    if source == "colorado"
+     facility = Facility.new({name: location[:dmv_office], 
+     address: location[:address_li], phone: location[:phone],
+     services: location[:services_p].split(",").map(&:strip)})
+    elsif source == "new_york"
+     facility = Facility.new({name: location[:office_name], address: location[:street_address_line_1], 
+     phone: location[:public_phone_number]})
+    elsif source == "missouri"
+      facility = Facility.new({name: location[:name], address: location[:address1], 
+     phone: location[:phone]})
+    end
     end
   end
 end
